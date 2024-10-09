02c2e115181ec8bfa9a4de492d5b6091ae7d3286
aconitum001 <skandern0000@gmail.com>
2024-10-09 13:51:23 +0100

    created assets class & added assets

---
+++ lib/utils/assets.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesBook1
+  /// assets/images/book1.png
+  static const String assetsImagesBook1 = "assets/images/book1.png";
+
+  /// Assets for assetsImagesBook2
+  /// assets/images/book2.png
+  static const String assetsImagesBook2 = "assets/images/book2.png";
+
+  /// Assets for assetsImagesBook3
+  /// assets/images/book3.png
+  static const String assetsImagesBook3 = "assets/images/book3.png";
+
+  /// Assets for assetsImagesBook4
+  /// assets/images/book4.png
+  static const String assetsImagesBook4 = "assets/images/book4.png";
+
+  /// Assets for assetsImagesBook5
+  /// assets/images/book5.png
+  static const String assetsImagesBook5 = "assets/images/book5.png";
+
+  /// Assets for assetsImagesBook6
+  /// assets/images/book6.png
+  static const String assetsImagesBook6 = "assets/images/book6.png";
+
+  /// Assets for assetsImagesBook7
+  /// assets/images/book7.png
+  static const String assetsImagesBook7 = "assets/images/book7.png";
+
+  /// Assets for assetsImagesBook8
+  /// assets/images/book8.png
+  static const String assetsImagesBook8 = "assets/images/book8.png";
+
+  /// Assets for assetsImagesMainBook
+  /// assets/images/main_book.png
+  static const String assetsImagesMainBook = "assets/images/main_book.png";
+}
+
