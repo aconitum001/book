83329d507e022be4134e4f3de56e39a7caab53ef
aconitum001 <skandern0000@gmail.com>
2024-10-09 13:58:48 +0100

    created app colors file

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color textColor = Color(0xff1C1B1F);
+  static Color lightText = const Color(0xff3C3C3D).withOpacity(.6);
+  static const Color primaryColor = Color(0xff9548D1);
+  static const Color backgroundColor = Color(0xffFEF8F0);
+  static const Color lightBlue = Color(0xff509FF8);
+  static const Color greyColor = Color(0xffEBE7E3);
+  static const Color orange1 = Color(0xffFFC85D);
+  static const Color orange2 = Color(0xffF89167);
+}
