6e8dbd0ae4075bd9ba9ae327014cc7af76f8b057
aconitum001 <skandern0000@gmail.com>
2024-10-09 13:43:45 +0100

    created app styles class

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,6 +1,96 @@
 import 'package:flutter/material.dart';
 
-abstract class AppStyles {}
+abstract class AppStyles {
+  static TextStyle style12Medium(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 12, context: context),
+      color: color,
+      fontWeight: FontWeight.w500, // Medium
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style14SemiBold(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 14, context: context),
+      color: color,
+      fontWeight: FontWeight.w600, // Semi-bold
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style15Medium(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 15, context: context),
+      color: color,
+      fontWeight: FontWeight.w500, // Medium
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style17SemiBold(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 17, context: context),
+      color: color,
+      fontWeight: FontWeight.w600, // Semi-bold
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style14Regular(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 14, context: context),
+      color: color,
+      fontWeight: FontWeight.w400, // Regular
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style16Regular(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 16, context: context),
+      color: color,
+      fontWeight: FontWeight.w400, // Regular
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style20SemiBold(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 20, context: context),
+      color: color,
+      fontWeight: FontWeight.w600, // Semi-bold
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style24SemiBold(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 24, context: context),
+      color: color,
+      fontWeight: FontWeight.w600, // Semi-bold
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style18SemiBold(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 18, context: context),
+      color: color,
+      fontWeight: FontWeight.w600, // Semi-bold
+      fontFamily: 'Rubik',
+    );
+  }
+
+  static TextStyle style13Regular(BuildContext context, {Color? color}) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(fontsize: 13, context: context),
+      color: color,
+      fontWeight: FontWeight.w400, // Regular
+      fontFamily: 'Rubik',
+    );
+  }
+}
 
 double getResponsiveFontSize(
     {required double fontsize, required BuildContext context}) {
